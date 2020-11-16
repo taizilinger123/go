9baa59a75a9a763d8c9202b8fdc58ba8f1b29738
sige <837337164@qq.com>
2020-11-16 22:29:46 +0800

    2020-11-16

--- src/go_dev/day4/example/example5/main.go
+++ src/go_dev/day4/example/example5/main.go
@@ -2,15 +2,14 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
 func recusive(n int) {
-	fmt.Println("hello")
-	time.Sleep(time.Second)
+	fmt.Println("hello", n)
+	/*time.Sleep(time.Second)
 	if n > 10 {
 		return
-	}
+	}*/
 	recusive(n + 1)
 }
 
