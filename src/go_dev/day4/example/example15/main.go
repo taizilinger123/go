61d78117e05e2444067d5b9b5803fd6481e12dd5
sige <837337164@qq.com>
2020-11-23 03:40:37 +0800

    2020-11-23

--- src/go_dev/day4/example/example15/main.go
+++ src/go_dev/day4/example/example15/main.go
@@ -83,4 +83,81 @@ func main() {
 
 //PS D:\project> go build -race  go_dev/day4/example/example15
 //PS D:\project> example15.exe
-//go  get github.com/go-sql-driver/mysql   //下载包
+//go  get github.com/go-sql-driver/mysql   //下载包,这个下载包都会下载到D:\project\src目录里
+/*
+D:\project>go  build -race go_dev/day4/example/example15
+
+D:\project>dir
+ 驱动器 D 中的卷是 Data
+ 卷的序列号是 DC8C-C090
+
+ D:\project 的目录
+
+2020/11/23  03:28    <DIR>          .
+2020/11/23  03:28    <DIR>          ..
+2020/11/14  22:46    <DIR>          .vscode
+2020/11/14  22:39    <DIR>          bin
+2020/11/23  03:26         2,137,600 example1.exe
+2020/11/23  03:31         3,195,392 example15.exe
+2020/11/23  01:51         3,193,856 main.exe
+2020/11/23  02:57    <DIR>          pkg
+2020/11/23  03:10    <DIR>          src
+2020/11/23  03:21    <DIR>          vender
+               3 个文件      8,526,848 字节
+               7 个目录 77,601,824,768 可用字节
+
+D:\project>example15.exe
+1726
+---------------------------------------------------------
+win10环境变量设置:
+shenglei的用户变量
+GOPATH   D:\project
+系统变量
+PATH:新增
+D:\go\bin
+---------------------------------------------------------
+D:\project>go  env
+set GO111MODULE=
+set GOARCH=amd64
+set GOBIN=
+set GOCACHE=C:\Users\shenglei\AppData\Local\go-build
+set GOENV=C:\Users\shenglei\AppData\Roaming\go\env
+set GOEXE=.exe
+set GOFLAGS=
+set GOHOSTARCH=amd64
+set GOHOSTOS=windows
+set GOINSECURE=
+set GOMODCACHE=D:\project\pkg\mod
+set GONOPROXY=
+set GONOSUMDB=
+set GOOS=windows
+set GOPATH=D:\project      ***********************
+set GOPRIVATE=
+set GOPROXY=https://goproxy.cn,direct
+set GOROOT=D:\go           *********************
+set GOSUMDB=sum.golang.org
+set GOTMPDIR=
+set GOTOOLDIR=D:\go\pkg\tool\windows_amd64
+set GCCGO=gccgo
+set AR=ar
+set CC=gcc
+set CXX=g++
+set CGO_ENABLED=1
+set GOMOD=
+set CGO_CFLAGS=-g -O2
+set CGO_CPPFLAGS=
+set CGO_CXXFLAGS=-g -O2
+set CGO_FFLAGS=-g -O2
+set CGO_LDFLAGS=-g -O2
+set PKG_CONFIG=pkg-config
+set GOGCCFLAGS=-m64 -mthreads -fno-caret-diagnostics -Qunused-arguments -fmessage-length=0 -fdebug-prefix-map=C:\Users\shenglei\AppData\Local\Temp\go-build182248274=/tmp/go-build -gno-record-gcc-switches
+---------------------------------------------------------------------
+D:\project 的目录
+2020/11/23  03:32    <DIR>          .
+2020/11/23  03:32    <DIR>          ..
+2020/11/14  22:46    <DIR>          .vscode
+2020/11/14  22:39    <DIR>          bin
+2020/11/23  02:57    <DIR>          pkg
+2020/11/23  03:10    <DIR>          src
+2020/11/23  03:21    <DIR>          vender
+*/
