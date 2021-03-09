d57f3d198f5adb095ee96adcdc26dee29cf4cae6
sige <837337164@qq.com>
2021-03-09 22:11:09 +0800

    20210309

--- src/go_dev/day5/example/example3/main.go
+++ src/go_dev/day5/example/example3/main.go
@@ -19,10 +19,11 @@ func trans(p *Student) {
 	}
 }
 
-func insertTail(p *Student){
+//尾部插入，正序
+func insertTail(p *Student) {
 	var tail = p
 	for i := 0; i < 10; i++ {
-		 stu := Student{
+		stu := Student{
 			Name:  fmt.Sprintf("stu%d", i),
 			Age:   rand.Intn(100),
 			Score: rand.Float32() * 100,
@@ -33,12 +34,62 @@ func insertTail(p *Student){
 	}
 }
 
+//头部插入,倒叙
+func insertHead(p **Student) {
+	//var tail = p
+	for i := 0; i < 10; i++ {
+		stu := Student{
+			Name:  fmt.Sprintf("stu%d", i),
+			Age:   rand.Intn(100),
+			Score: rand.Float32() * 100,
+		}
+		stu.next = *p
+		*p = &stu
+		//tail.next = &stu
+		//tail = &stu
+	}
+}
+
+func delNode(p *Student) {
+	var prev *Student = p
+	for p != nil {
+		if p.Name == "stu6" {
+			prev.next = p.next
+			break
+		}
+		prev = p
+		p = p.next
+	}
+
+}
+
+func addNode(p *Student, newNode *Student){
+	for p != nil {
+		if p.Name == "stu5" {
+			newNode.next = p.next
+			p.next = newNode
+			break
+		}
+		p = p.next
+	}
+}
+
 func main() {
-	var head Student
+	var head *Student = new(Student)
 	head.Name = "hua"
 	head.Age = 18
 	head.Score = 100
 
-    insertTail(&head)
-	trans(&head)
+	//insertTail(head)
+	//trans(head)
+	insertHead(&head)
+	trans(head)
+	delNode(head)
+	trans(head)
+	var newNode *Student = new(Student)
+	newNode.Name = "stu1000"
+	newNode.Age = 18
+	newNode.Score = 100
+	addNode(head, newNode)
+	trans(head)
 }
