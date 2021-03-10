3f4da7bf56a825d5282dd190328bdc5bd609f148
sige <837337164@qq.com>
2021-03-11 00:00:55 +0800

    20210311

---
+++ src/go_dev/day5/example/example10/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Test interface {
+	Print()
+}
+
+type Student struct {
+	 name  string 
+	 age   int 
+	 score int
+}
+
+func (p *Student) Print(){
+	fmt.Println("name", p.name)
+	fmt.Println("age", p.age)
+	fmt.Println("score", p.score)
+}
+
+func main(){
+	var  t Test 
+	var stu Student = Student{
+		name: "stu1",
+		age:  20,
+		score: 200,
+	}
+
+	t = &stu 
+	t.Print()
+}
