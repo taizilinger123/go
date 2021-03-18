7a8c5f60234dc4baf567837de2d31bb6a0eec35c
sige <837337164@qq.com>
2021-03-18 14:41:01 +0800

    20210318

---
+++ src/go_dev/day6/example3/main3.go
@@ -0,0 +1,58 @@
+package main
+
+import "fmt"
+
+type Carer interface {
+	GetName() string
+	Run()
+	DiDi()
+}
+
+type BMW struct {
+	Name string
+}
+
+func (p *BMW) GetName() string {
+	return p.Name
+}
+
+func (p *BMW) Run() {
+	fmt.Printf("%s is running\n", p.Name)
+}
+
+func (p *BMW) DiDi() {
+	fmt.Printf("%s is didi\n", p.Name)
+}
+
+type BYD struct {
+	Name string
+}
+
+func (p *BYD) GetName() string {
+	return p.Name
+}
+
+func (p *BYD) Run() {
+	fmt.Printf("%s is running\n", p.Name)
+}
+
+func (p *BYD) DiDi() {
+	fmt.Printf("%s is didi\n", p.Name)
+}
+
+func main() {
+	var car Carer
+	fmt.Println(car)
+
+	bmw := &BMW{
+		Name: "BMW",
+	}
+	car = bmw
+	car.Run()
+
+	byd := &BYD{
+		Name: "BYD",
+	}
+	car = byd
+	car.Run()
+}
