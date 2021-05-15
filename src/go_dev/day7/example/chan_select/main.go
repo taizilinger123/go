07106628d1bee556932ad4ee27946a0dfe1e6b0a
sige <837337164@qq.com>
2021-05-15 16:44:26 +0800

    20210515

---
+++ src/go_dev/day7/example/chan_select/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	var ch chan int
+	ch = make(chan int, 10)
+
+	for i := 0; i < 10; i++ {
+		ch <- i
+	}
+
+	for {
+		select {
+		case v := <-ch:
+			fmt.Println(v)
+		default:
+			fmt.Println("get data time out")
+			time.Sleep(time.Second)
+		}
+	}
+}
